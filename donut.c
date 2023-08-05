85171f9e2ccaa426884d5a0abdbf8dd588c79f0c
liamholland <liamholland02@gmail.com>
2023-08-05 17:44:35 +0100

    study of older version which uses math.h

--- donut.c
+++ donut.c
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <Windows.h>
 
+//this version does not require a math library but i have not studied it yet
+/*
 #define R(mul, shift, x, y)            \
     _ = x;                             \
     x -= mul * y >> shift;             \
@@ -55,8 +58,59 @@ int main()
             putchar(k % 80 ? b[k] : 10);
         R(5, 7, cA, sA);
         R(5, 8, cB, sB);
-        // Sleep(10);
-        system("cls");
+        system("cls");  //clear the screen
+    }
+    return 0;
+}*/
+
+int k;
+double sin(), cos();
+int main()
+{
+    float A = 0, B = 0; //initialise the multipliers for rotations - these represent the axes about which the donut spins
+    float z[1760];  //z buffer
+    char b[1760];   //the donut
+    system("cls");  //clear the screen
+    while(1) {
+        //empty the arrays
+        memset(b, 32, 1760);
+        memset(z, 0, 7040); //this is the z-buffer - map the values over some multiple of the size of z times
+        //for each point on the taurus
+        for (float j = 0; j < 6.28; j += 0.07)    //6.28 = 2pi, theta spacing
+            for (float i = 0; i < 6.28; i += 0.02)    //6.28 = 2pi, phi spacing
+            {
+                //create the donut
+                float c = sin(i), 
+                    d = cos(j), 
+                    e = sin(A), 
+                    f = sin(j), 
+                    g = cos(A), 
+                    h = d + 3, //the constant here will scale the donut
+                    D = 2 / (c * h * e + f * g + 5), //the constant will determine how far away the donut is from the viewer
+                    l = cos(i), 
+                    m = cos(B), 
+                    n = sin(B),
+                    t = c * h * g - f * e;
+
+                //finding the x and y values
+                int x = 40 + 30 * D * (l * h * m - t * n),
+                    y = 12 + 15 * D * (l * h * n + t * m), 
+                    
+                    o = x + 80 * y, //convert x and y into points in the array
+                    N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);  //lighting
+
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o])
+                {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        printf("\x1b[H");   //put the cursor back to the start of the screen to overwrite whats there
+        //80 characters per line
+        for (k = 0; k < 1761; k++)
+            putchar(k % 80 ? b[k] : 10);
+        A += 0.04;
+        B += 0.02;
     }
     return 0;
 }
